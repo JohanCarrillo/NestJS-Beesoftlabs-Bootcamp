867da06cd25929a0f1b201e4b3d28c046a4e646c
JohanCarrillo <johanime1907@gmail.com>
2022-08-27 12:53:01 -0500

    added different ways of handling a post request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,98 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Get,
+	Param,
+	Post,
+	Query,
+	UsePipes,
+	ValidationPipe,
+} from "@nestjs/common";
+import { plainToInstance } from "class-transformer";
 import { AppService } from "./app.service";
+import { CreateUserDto } from "./createUser.dto";
 import { User } from "./database/entities/user.entity";
+import { ReadUserDto } from "./readUser.dto";
 
 export interface IResponse {
 	appName: string;
 	appUrl: string;
 	appPort: number;
 }
-@Controller()
+@Controller("user")
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Get()
+	@Get("hello")
 	getHello(): IResponse {
 		return this.appService.getHello();
 	}
 
-	@Get("users/")
+	@Get("/")
 	async getAllUsers(): Promise<User[]> {
 		return await this.appService.getAllUsers();
 	}
 
-	@Get("users/:userId")
+	@Get("/:userId")
 	async getUser(@Param("userId") userId: number): Promise<User> {
 		return await this.appService.getUser(userId);
+	}
+
+	@Post("/add/queries")
+	createUserQueries(
+		@Query("name") name: string,
+		@Query("address") address: string,
+		@Query("email") email: string,
+		@Query("age") age: number
+	): Promise<User> {
+		return this.appService.createUser(name, address, email, age);
+	}
+
+	@Post("add/params/name/:name/address/:address/email/:email/age/:age")
+	createUserParams(
+		@Param("name") name: string,
+		@Param("address") address: string,
+		@Param("email") email: string,
+		@Param("age") age: number
+	): Promise<User> {
+		return this.appService.createUser(name, address, email, age);
+	}
+
+	@Post("add/body")
+	createUserBody(
+		@Body("name") name: string,
+		@Body("address") address: string,
+		@Body("email") email: string,
+		@Body("age") age: number
+	): Promise<User> {
+		return this.appService.createUser(name, address, email, age);
+	}
+
+	@Post("add/dto/destructure")
+	@UsePipes(new ValidationPipe({ transform: true }))
+	createUserBodyDtoDestructured(
+		@Body() createUserDto: CreateUserDto
+	): Promise<User> {
+		return this.appService.createUser(
+			createUserDto.name,
+			createUserDto.address,
+			createUserDto.email,
+			createUserDto.age
+		);
+	}
+
+	@Post("add/dto")
+	@UsePipes(new ValidationPipe({ transform: true }))
+	createUserDto(@Body() createUserDto: CreateUserDto): Promise<User> {
+		return this.appService.createUserWithDto(createUserDto);
+	}
+
+	@Post("add/dto/responseDto")
+	@UsePipes(new ValidationPipe({ transform: true }))
+	createUserDtoResponseDto(@Body() createUserDto: CreateUserDto): ReadUserDto {
+		const response = this.appService.createUserWithDto(createUserDto);
+		return plainToInstance(ReadUserDto, response, {
+			excludeExtraneousValues: true,
+		});
 	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { IResponse } from "./app.controller";
 import { AppConfigService } from "./config/app/config.service";
+import { CreateUserDto } from "./createUser.dto";
 import { User } from "./database/entities/user.entity";
 
 @Injectable()
@@ -26,5 +27,16 @@ export class AppService {
 
 	getUser(userId: number): Promise<User> {
 		return this.userRepo.findOne(userId);
+	}
+
+	async createUser(name: string, address: string, email: string, age: number) {
+		return await this.userRepo.save({ name, address, email, age });
+	}
+
+	async createUserWithDto(createUserDto: CreateUserDto) {
+		return await this.userRepo.save({ ...createUserDto });
+	}
+	async createUserWithDtoResponseWithDto(createUserDto: CreateUserDto) {
+		return await this.userRepo.save({ ...createUserDto });
 	}
 }
